81f59b68788e2a9900ae0afb7b11208f574d640a
muntazir09 <113083548+muntazir09@users.noreply.github.com>
2022-10-27 14:10:51 -0700

    commit to main

---
+++ Chapter 3- Data Structures/Exercises/Ch 3 Q 3.py
@@ -0,0 +1,7 @@
+## Exercise 3: Your Own List
+
+transportation = ['car', 'motorcycle', 'airplane', 'bus,']
+print(transportation[0] + ' is a very comfortable means of transportation.')
+print(transportation[1] + ' is a decent mean of transportation for small distances.')
+print(transportation[2] + ' is a very reliable transportation for long distances.')
+print(transportation[3] + ' I use it for my uni transportation.')
---
+++ Chapter 3- Data Structures/Exercises/Ch 3 Q 4.py
@@ -0,0 +1,6 @@
+## Exercise 4: Guest List
+
+Invitation = ['Friends ', 'Neighbours ', 'Cousins ']
+print(Invitation[0] + 'sending you the heartiest invitation for the dinner.')
+print(Invitation[1] + 'sending you the heartiest invitation for the dinner.')
+print(Invitation[2] + 'sending you the heartiest invitation for the dinner.')
