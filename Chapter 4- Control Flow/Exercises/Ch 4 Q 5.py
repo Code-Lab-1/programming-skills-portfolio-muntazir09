11de96e9cf71fb5c7e3647d10a0c6d7083c7a53d
muntazir09 <113083548+muntazir09@users.noreply.github.com>
2022-10-28 05:14:16 -0700

    Commit to main

---
+++ Chapter 4- Control Flow/Exercises/Ch 4 Q 5.py
@@ -0,0 +1,13 @@
+# Exercise 5: Favorite Fruit
+
+Favorite_Fruit = ['Strawberry', 'Apple', 'Mango']
+if 'Strawberry' in Favorite_Fruit:
+    print("You really like Strawberry")
+if 'banana' in Favorite_Fruit:
+    print("You really like banana")
+if 'Apple' in Favorite_Fruit:
+    print("You really like Apple")
+if 'grapes' in Favorite_Fruit:
+    print("You really like grapes")
+if 'Mango' in Favorite_Fruit:
+    print("You really like Mango")
